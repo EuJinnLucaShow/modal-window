1bc1de84581eab5387ec7a377134010224f04f0d
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-04-17 19:32:28 +0300

    Summary

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnShow = document.querySelectorAll('.show-modal-window');
+const modalWindow = document.querySelector('.modal-window');
+const btnClose = document.querySelector('.close-modal-window');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnShow.length; i++) {
+  btnShow[i].addEventListener('click', () => {});
+}
